17da74e8110370068633d4dd48cf0218da94c051
Thalysson Henrique da Silva Lourenço <thalysson@pop-os.localdomain>
2023-06-07 11:46:11 -0300

    refactor: round robin algorithm

--- apps/entity_resolution/lib/entity_resolution/algorithms/round_robin.ex
+++ apps/entity_resolution/lib/entity_resolution/algorithms/round_robin.ex
@@ -3,22 +3,11 @@ defmodule EntityResolution.Algorithms.RoundRobin do
 
   @impl true
   def init(servers) do
-    {:ok, {servers, 0}}
+    {:ok, servers}
   end
 
   @impl true
-  def handle_call(:get_next_server, _from, {[%{name: current_server} | _] = servers, 0}) do
-    {:reply, current_server, {servers, 1}}
-  end
-
-  def handle_call(:get_next_server, _from, {servers, current_server_index})
-      when length(servers) == current_server_index + 1 do
-    {:reply, servers |> List.last(servers) |> Map.fetch!(:name), {servers, 0}}
-  end
-
-  def handle_call(:get_next_server, _from, {servers, current_server_index}) do
-    current_server = servers |> Enum.at(current_server_index) |> Map.fetch!(:name)
-    next_server_index = current_server_index + 1
-    {:reply, current_server, {servers, next_server_index}}
+  def handle_call(:get_next_server, _from, [%{name: current_server} = server | servers]) do
+    {:reply, current_server, servers ++ [server]}
   end
 end
